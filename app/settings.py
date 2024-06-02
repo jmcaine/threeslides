f29a8a6f164d5b3f6649a8b2e4832b6506ff8c0f
jmcaine <jmcaine@caines.us>
2024-06-01 21:41:26 -0700

    cleanup

--- app/settings.py
+++ app/settings.py
@@ -8,30 +8,3 @@ debug = True
 
 # deployment port:
 k_port = 8080
-
-
-
-#k_http = 'http'
-#k_ws = 'ws'
-#k_ws_url_prefix = ''
-#k_static_url = 'http://10.20.34.58:8001/static/' # for use with adev
-#k_static_url = 'http://10.20.60.80:8001/static/' # for use with adev
-#k_static_url = 'http://10.20.50.90:8001/static/' # for use with adev
-#k_static_url = 'http://10.20.60.159:8001/static/' # for use with adev
-###k_static_url = 'http://localhost:8001/static/' # for use with adev
-###k_static_url = 'static/' # for use with adev
-#k_static_url = 'http://localhost:8080/static/' # for use with stright python -m aiohttp.web
-
-#k_host = '10.20.34.58:8000'
-#k_host = '10.20.60.80:8000'
-#k_host = '10.20.50.90:8000'
-#k_host = '10.20.60.159:8000'
-###k_host = 'localhost:8000'
-
-# deploy:
-#debug = False
-#k_http = 'https'
-#k_ws = 'wss'
-#k_ws_url_prefix = '/ws' # '/a_ws'
-#k_static_url = 'https://openhome.school' + k_url_prefix + '/static/'
-#k_host = 'openhome.school'
