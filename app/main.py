1c72429ceec43ab14e5ceb059167868da496b152
jmcaine <jmcaine@caines.us>
2025-02-07 17:44:19 -0800

    added png support and movie downsizing

--- app/main.py
+++ app/main.py
@@ -796,11 +796,12 @@ async def _ws_binary(hd, data):
 			vid.set(cv2.CAP_PROP_POS_FRAMES, int(vid.get(cv2.CAP_PROP_FRAME_COUNT)) / 2) # get thumbnail from half-way frame (because the first umpteen are often black, in a fade-from-black)
 			result, image = vid.read()
 			assert result == True, "FAILED to capture frame!"
-			cv2.imwrite(fp + k_thumb_appendix, image)
+			thumb = cv2.resize(image, (k_thumbnail_size, int(k_thumbnail_size * 10 / 16)))
+			cv2.imwrite(fp + k_thumb_appendix, thumb)
 		elif name.lower().endswith(k_image_formats):
 			img = Image.open(io.BytesIO(payload[pos:pos+size]))
 			img.save(fp)
-			img.thumbnail((300, 300)) # modifies img in-place
+			img.thumbnail((k_thumbnail_size, k_thumbnail_size)) # modifies img in-place
 			img.save(fp + k_thumb_appendix)
 		elif name.lower().endswith(k_audio_formats):
 			with open(fp, "wb") as file:
--- app/shared.py
+++ app/shared.py
@@ -1,3 +1,6 @@
 k_video_formats = ('.mp4', '.mov', '.mkv', '.mov')
 k_audio_formats = ('.m4a', '.mp3', )
-k_image_formats = ('.jpg', '.jpeg', ) # png?
+k_image_formats = ('.jpg', '.jpeg', '.png' ) # png?
+k_thumbnail_size = 300
+
+
